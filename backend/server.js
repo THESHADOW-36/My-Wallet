979473115bb36346d763acdf3c519532b7d8c40a
DINESH THIRU <dineshthiru36@gmail.com>
2024-05-09 02:14:55 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('tiny'))
 app.use(cors());
-config({ path: './config/config.env' });
+config({ path: './config/.env' });
 connectDB();
 
 app.use('/api/v1',router);

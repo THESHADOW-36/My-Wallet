2c88921f29dfed7df6f659909c97d2d427a3d4a4
DINESH THIRU <dineshthiru36@gmail.com>
2024-05-09 02:33:48 +0530

    updated

--- frontend/src/constant/Url.tsx
+++ frontend/src/constant/Url.tsx
@@ -1,26 +1,28 @@
+// http://localhost:8000
+
 // Auth
-export const register = "http://localhost:8000/api/v1/auth/register"  // Post
-export const login = "http://localhost:8000/api/v1/auth/login"  // Post
-export const currentUser = "http://localhost:8000/api/v1/auth/current-user"  // get
-export const editProfile = "http://localhost:8000/api/v1/auth/edit-profile"  // put
+export const register = "https://my-wallet-gwsi.onrender.com/api/v1/auth/register"  // Post
+export const login = "https://my-wallet-gwsi.onrender.com/api/v1/auth/login"  // Post
+export const currentUser = "https://my-wallet-gwsi.onrender.com/api/v1/auth/current-user"  // get
+export const editProfile = "https://my-wallet-gwsi.onrender.com/api/v1/auth/edit-profile"  // put
 
 // Expenses
-export const addExp = "http://localhost:8000/api/v1/wallet/expenses"  // Post
-export const getExp = "http://localhost:8000/api/v1/wallet/expenses"  // get
-export const getSingleExp = "http://localhost:8000/api/v1/wallet/expense/"  // get
-export const editExp = "http://localhost:8000/api/v1/wallet/expense/"  // put
-export const delExp = "http://localhost:8000/api/v1/wallet/expense/"  // delete
+export const addExp = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/expenses"  // Post
+export const getExp = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/expenses"  // get
+export const getSingleExp = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/expense/"  // get
+export const editExp = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/expense/"  // put
+export const delExp = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/expense/"  // delete
 
 // Incomes
-export const addIncome = "http://localhost:8000/api/v1/wallet/incomes"  // Post
-export const getIncome = "http://localhost:8000/api/v1/wallet/incomes"  // get
-export const getSingleIncome = "http://localhost:8000/api/v1/wallet/income/"  // get
-export const editIncome = "http://localhost:8000/api/v1/wallet/income/"  // put
-export const delIncome = "http://localhost:8000/api/v1/wallet/income/"  // delete
+export const addIncome = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/incomes"  // Post
+export const getIncome = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/incomes"  // get
+export const getSingleIncome = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/income/"  // get
+export const editIncome = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/income/"  // put
+export const delIncome = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/income/"  // delete
 
 // Statistics
-export const stats = "http://localhost:8000/api/v1/wallet/stats"  // get
-export const chartStats = "http://localhost:8000/api/v1/wallet/chart-stats"  // get
+export const stats = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/stats"  // get
+export const chartStats = "https://my-wallet-gwsi.onrender.com/api/v1/wallet/chart-stats"  // get
 
 export const Url = {
    register,
